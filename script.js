3901f8f1de62c7890fabd9480133543f092a2ec7
Yaniv Izaki <Yaniv Izaki>
2016-11-15 20:06:27 +0200

    .

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+var newBook = {
+	bookName: "The Hunger Games",
+	time: 25,
+	desc: "The Hunger Games takes place in a nation known as Panem, established in North America after the destruction of the continent's civilization by an unknown apocalyptic event.",
+	author: "Suzanne Collins",
+	image: "https://upload.wikimedia.org/wikipedia/en/d/dc/The_Hunger_Games.jpg"
+}
 
 
+var source = $('#book-template').html();
+var template = Handlebars.compile(source);
+var newHTML = template(newBook);
+$('#info-book').append(newHTML);
